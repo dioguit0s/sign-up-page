f3ed67300e8b03d60b016a6a432107114eaa62f8
dioguit0s <diogosantos152005@gmail.com>
2023-02-17 16:29:58 -0300

    validação email

---
+++ js/scriptCadastro.js
@@ -0,0 +1,35 @@
+//função start que puxa todas as outras ao clique do botão
+function login() {
+  recebeEmail();
+  recebeNome();
+  recebeSobrenome();
+}
+
+//função que recebe e verifica o email
+function recebeEmail() {
+  var inputEmail = document.querySelector('#email');
+  var email = String(inputEmail.value);
+
+  validarEmail(email);
+}
+
+//função que valida a formatação do email
+function validarEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+
+  if (!re.test(email)) {
+    alert('email invalido');
+  }
+  return re.test(email);
+}
+
+//funçao que recebe o nome e guarda no localStorage
+function recebeNome() {
+  var primeiroNome = document.querySelector('#nome');
+  console.log(primeiroNome);
+}
+
+//funçao que recebe o sobrenome e guarda no localStorage
+function recebeSobrenome() {
+  var segundoNome = document.querySelector('#sobrenome');
+}
